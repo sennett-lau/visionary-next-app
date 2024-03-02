7d1332a85c34ca5710e3a5af18c0aec4fd134dce
Sennett Lau <laub1199@gmail.com>
2024-03-02 17:04:22 +0800

    added learning slice implementation

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit'
 import { setupListeners } from '@reduxjs/toolkit/query'
 
 import controlSlice from '@/store/controlSlice'
+import learningSlice from '@/store/learningSlice'
 
 const store = configureStore({
   reducer: {
     // Add the generated reducer as a specific top-level slice
     controlSlice: controlSlice.reducer,
+    learningSlice: learningSlice.reducer,
   },
 })
 
